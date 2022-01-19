7f4f971ac19016df94150c10613fc3ee1ac01eaa
WooMinGy <alsrl1110@naver.com>
2022-01-19 14:20:07 +0900

    UPDATE 채팅방 스피너 추가

--- project-11/src/components/OAuthRedirect.js
+++ project-11/src/components/OAuthRedirect.js
@@ -15,16 +15,14 @@ const OAuthRedirect = () => {
     axiosInstance
       .get(`/oauth/callback/kakao?code=${code}`)
       .then((response) => {
-        // console.log(response);
+        console.log(response);
         const address = response.data.address;
-        // console.log(address);
-        // const loginInfo = `userId=${response.data.userId}userImg=${response.data.profileImg}userName=${response.data.nickName}userToken=Bearer ${response.data.token}`;
-        // setCookie("OK", loginInfo);
         setCookie("userId", response.data.userId);
         setCookie("userImg", response.data.profileImg);
         let name = encodeURIComponent(response.data.nickName);
         setCookie("userName", name);
         setCookie("userToken", `Bearer ${response.data.token}`);
+        console.log(address);
         if (address !== null) {
           history.push("/main");
         } else {
