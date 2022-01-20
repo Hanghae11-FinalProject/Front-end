b106fd3a0233db1bcf4f5d2ca4b623f79a4205fb
leejh4197 <leejh4197@naver.com>
2022-01-21 01:40:53 +0900

    [TEST]

--- project-11/src/pages/Chat.js
+++ project-11/src/pages/Chat.js
@@ -483,6 +483,7 @@ const ChatInput = styled.div`
     display: flex;
     align-items: center;
     background-color: black;
+    justify-content: space-between;
     input {
       width: 305px;
       height: 40px;
